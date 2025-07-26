0fa829d316a24378d85944767b3683e955ae85d3
wcyiming <wcyiming@outlook.com>
2025-07-25 23:26:48 -0400

    Origin version

---
+++ Source/Aurora/Aurora.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Aurora.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aurora, "Aurora" );
